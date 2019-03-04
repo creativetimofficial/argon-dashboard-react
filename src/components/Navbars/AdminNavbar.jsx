151ae6a1fedb555ff0eb9634cc192198bc679f6c
Manu <emanuelnazare@yahoo.ro>
2019-03-04 16:49:22 +0200

    - Navbar done

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 // reactstrap components
 import {
   DropdownMenu,
   DropdownItem,
   UncontrolledDropdown,
+  DropdownToggle,
   Form,
   FormGroup,
   InputGroupAddon,
@@ -12,7 +13,6 @@ import {
   Input,
   InputGroup,
   Navbar,
-  NavLink,
   Nav,
   Container,
   Media
@@ -30,7 +30,6 @@ class AdminNavbar extends React.Component {
             >
               Dashboard
             </a>
-
             <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
               <FormGroup className="mb-0">
                 <InputGroup className="input-group-alternative">
@@ -39,22 +38,15 @@ class AdminNavbar extends React.Component {
                       <i className="fas fa-search" />
                     </InputGroupText>
                   </InputGroupAddon>
-
                   <Input placeholder="Search" type="text" />
                 </InputGroup>
               </FormGroup>
             </Form>
-
             <Nav className="align-items-center d-none d-md-flex" navbar>
               <UncontrolledDropdown nav>
-                <NavLink
-                  aria-expanded={false}
-                  aria-haspopup={true}
+                <DropdownToggle
                   className="pr-0"
-                  data-toggle="dropdown"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                  role="button"
+                  nav
                 >
                   <Media className="align-items-center">
                     <span className="avatar avatar-sm rounded-circle">
@@ -63,49 +55,36 @@ class AdminNavbar extends React.Component {
                         src={require("assets/img/theme/team-4-800x800.jpg")}
                       />
                     </span>
-
                     <Media className="ml-2 d-none d-lg-block">
                       <span className="mb-0 text-sm font-weight-bold">
                         Jessica Jones
                       </span>
                     </Media>
                   </Media>
-                </NavLink>
-
+                </DropdownToggle>
                 <DropdownMenu className="dropdown-menu-arrow" right>
                   <DropdownItem className="noti-title" header tag="div">
                     <h6 className="text-overflow m-0">Welcome!</h6>
                   </DropdownItem>
-
-                  <DropdownItem href="./examples/profile.html">
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-single-02" />
-
                     <span>My profile</span>
                   </DropdownItem>
-
-                  <DropdownItem href="./examples/profile.html">
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-settings-gear-65" />
-
                     <span>Settings</span>
                   </DropdownItem>
-
-                  <DropdownItem href="./examples/profile.html">
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-calendar-grid-58" />
-
                     <span>Activity</span>
                   </DropdownItem>
-
-                  <DropdownItem href="./examples/profile.html">
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-support-16" />
-
                     <span>Support</span>
                   </DropdownItem>
-
                   <DropdownItem divider />
-
                   <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
                     <i className="ni ni-user-run" />
-
                     <span>Logout</span>
                   </DropdownItem>
                 </DropdownMenu>
