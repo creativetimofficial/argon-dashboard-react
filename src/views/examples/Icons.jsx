1954c2f846c6474aa298d6a6a6cb8f53f52e8db7
Manu <emanuelnazare@yahoo.ro>
2019-03-05 17:56:18 +0200

    - Deleted footer from example views

--- src/views/examples/Icons.jsx
+++ src/views/examples/Icons.jsx
@@ -1467,64 +1467,6 @@ class Icons extends React.Component {
               </Card>
             </div>
           </Row>
-          {/* Footer */}
-          <footer className="footer">
-            <Row className="align-items-center justify-content-xl-between">
-              <Col xl="6">
-                <div className="copyright text-center text-xl-left text-muted">
-                  © 2018{" "}
-                  <a
-                    className="font-weight-bold ml-1"
-                    href="https://www.creative-tim.com"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    Creative Tim
-                  </a>
-                </div>
-              </Col>
-              <Col xl="6">
-                <Nav className="nav-footer justify-content-center justify-content-xl-end">
-                  <NavItem>
-                    <NavLink
-                      href="https://www.creative-tim.com"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      Creative Tim
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://www.creative-tim.com/presentation"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      About Us
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="http://blog.creative-tim.com"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      Blog
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      MIT License
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-              </Col>
-            </Row>
-          </footer>
         </Container>
       </>
     );
