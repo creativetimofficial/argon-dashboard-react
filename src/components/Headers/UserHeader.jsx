303ed9f307d32bb41cb82606b26f8ba072a57db9
Manu <emanuelnazare@yahoo.ro>
2019-03-06 12:16:12 +0200

    - Created Header components

---
+++ src/components/Headers/UserHeader.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+// reactstrap components
+import { Button, Container, Row, Col } from "reactstrap";
+
+class UserHeader extends React.Component {
+  render() {
+    return (
+      <>
+        <div
+          className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
+          style={{
+            minHeight: "600px",
+            backgroundImage: "url(../assets/img/theme/profile-cover.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center top"
+          }}
+        >
+          {/* Mask */}
+          <span className="mask bg-gradient-default opacity-8" />
+          {/* Header container */}
+          <Container className="d-flex align-items-center" fluid>
+            <Row>
+              <Col lg="7" md="10">
+                <h1 className="display-2 text-white">Hello Jesse</h1>
+                <p className="text-white mt-0 mb-5">
+                  This is your profile page. You can see the progress you've
+                  made with your work and manage your projects or assigned tasks
+                </p>
+                <Button
+                  color="info"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  Edit profile
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </>
+    );
+  }
+}
+
+export default UserHeader;
--- src/views/examples/Profile.jsx
+++ src/views/examples/Profile.jsx
@@ -13,43 +13,14 @@ import {
   Row,
   Col
 } from "reactstrap";
+// core components
+import UserHeader from "components/Headers/UserHeader.jsx";
 
 class Profile extends React.Component {
   render() {
     return (
       <>
-        {/* Header */}
-        <div
-          className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
-          style={{
-            minHeight: "600px",
-            backgroundImage: "url(../assets/img/theme/profile-cover.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center top"
-          }}
-        >
-          {/* Mask */}
-          <span className="mask bg-gradient-default opacity-8" />
-          {/* Header container */}
-          <Container className="d-flex align-items-center" fluid>
-            <Row>
-              <Col lg="7" md="10">
-                <h1 className="display-2 text-white">Hello Jesse</h1>
-                <p className="text-white mt-0 mb-5">
-                  This is your profile page. You can see the progress you've
-                  made with your work and manage your projects or assigned tasks
-                </p>
-                <Button
-                  color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                >
-                  Edit profile
-                </Button>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+        <UserHeader />
         {/* Page content */}
         <Container className="mt--7" fluid>
           <Row>
