dbf2fe92bd8716a02dbf20b587931d2fdaea3746
Manu <emanuelnazare@yahoo.ro>
2019-03-07 10:25:04 +0200

    - Added comments for all imports

--- src/components/Footers/AdminFooter.jsx
+++ src/components/Footers/AdminFooter.jsx
@@ -1,7 +1,5 @@
 /*eslint-disable*/
 import React from "react";
-// used for making the prop types of this component
-import PropTypes from "prop-types";
 
 // reactstrap components
 import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+// node.js library that concatenates classes (strings)
 import classnames from "classnames";
+// javascipt plugin for creating charts
 import Chart from "chart.js";
 // react plugin used to create charts
 import { Line, Bar } from "react-chartjs-2";
