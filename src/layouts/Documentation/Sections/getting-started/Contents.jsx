1144b9c3f008fb0b184f2f7c919166ab2322ca27
Manu <emanuelnazare@yahoo.ro>
2019-03-07 18:00:25 +0200

    - Contents docs is done

--- src/layouts/Documentation/Sections/getting-started/Contents.jsx
+++ src/layouts/Documentation/Sections/getting-started/Contents.jsx
@@ -4,7 +4,113 @@ import { prism } from "react-syntax-highlighter/dist/styles/prism";
 // reactstrap components
 import { Card, Row, Col } from "reactstrap";
 
-const fileStructure = ``;
+const fileStructure = `Argon Dashboard React
+.
+├── Documentation
+│   └── documentation.html
+├── CHANGELOG.md
+├── ISSUE_TEMPLATE.md
+├── LICENSE
+├── README.md
+├── package.json
+├── public
+│   ├── favicon.ico
+│   ├── index.html
+│   └── manifest.json
+└── src
+    ├── assets
+    │   ├── css
+    │   │   ├── argon-dashboard-react.css
+    │   │   ├── argon-dashboard-react.css.map
+    │   │   └── argon-dashboard-react.min.css
+    │   ├── fonts
+    │   │   └── nucleo
+    │   ├── img
+    │   │   ├── brand
+    │   │   ├── icons
+    │   │   │   └── common
+    │   │   └── theme
+    │   ├── scss
+    │   │   ├── argon-dashboard-react.scss
+    │   │   ├── bootstrap
+    │   │   │   ├── mixins
+    │   │   │   └── utilities
+    │   │   ├── core
+    │   │   │   ├── alerts
+    │   │   │   ├── avatars
+    │   │   │   ├── badges
+    │   │   │   ├── buttons
+    │   │   │   ├── cards
+    │   │   │   ├── charts
+    │   │   │   ├── close
+    │   │   │   ├── custom-forms
+    │   │   │   ├── dropdowns
+    │   │   │   ├── footers
+    │   │   │   ├── forms
+    │   │   │   ├── headers
+    │   │   │   ├── icons
+    │   │   │   ├── list-groups
+    │   │   │   ├── maps
+    │   │   │   ├── masks
+    │   │   │   ├── mixins
+    │   │   │   ├── modals
+    │   │   │   ├── navbars
+    │   │   │   ├── navs
+    │   │   │   ├── paginations
+    │   │   │   ├── popovers
+    │   │   │   ├── progresses
+    │   │   │   ├── separators
+    │   │   │   ├── tables
+    │   │   │   ├── type
+    │   │   │   ├── utilities
+    │   │   │   └── vendors
+    │   │   ├── custom
+    │   │   └── react
+    │   └── vendor
+    │       ├── @fortawesome
+    │       │   └── fontawesome-free
+    │       │       ├── LICENSE.txt
+    │       │       ├── css
+    │       │       ├── js
+    │       │       ├── less
+    │       │       ├── scss
+    │       │       ├── sprites
+    │       │       ├── svgs
+    │       │       │   ├── brands
+    │       │       │   ├── regular
+    │       │       │   └── solid
+    │       │       └── webfonts
+    │       └── nucleo
+    │           ├── css
+    │           └── fonts
+    ├── components
+    │   ├── Footers
+    │   │   ├── AdminFooter.jsx
+    │   │   └── AuthFooter.jsx
+    │   ├── Headers
+    │   │   ├── Header.jsx
+    │   │   └── UserHeader.jsx
+    │   ├── Navbars
+    │   │   ├── AdminNavbar.jsx
+    │   │   └── AuthNavbar.jsx
+    │   └── Sidebar
+    │       └── Sidebar.jsx
+    ├── index.js
+    ├── layouts
+    │   ├── Admin.jsx
+    │   └── Auth.jsx
+    ├── routes.js
+    ├── variables
+    │   └── charts.jsx
+    └── views
+        ├── Index.jsx
+        └── examples
+            ├── Icons.jsx
+            ├── Login.jsx
+            ├── Maps.jsx
+            ├── Profile.jsx
+            ├── Register.jsx
+            └── Tables.jsx`;
 
 class Contents extends React.Component {
   render() {
@@ -28,9 +134,9 @@ class Contents extends React.Component {
         <SyntaxHighlighter language="html" style={prism}>
           {fileStructure}
         </SyntaxHighlighter>
-        <h2 id="bootstrap-components">Bootstrap components</h2>
+        <h2 id="bootstrap-components">Bootstrap / Reactstrap components</h2>
         <p>
-          Here is the list of Bootstrap 4 components that were restyled in
+          Here is the list of Bootstrap 4 / Reactstrap components that were restyled in
           Argon Dashboard React:
         </p>
         <Row className="row-grid mt-5">
@@ -123,7 +229,7 @@ class Contents extends React.Component {
             </Card>
           </Col>
         </Row>
-        <h2 id="argon-components">Argon components</h2>
+        <h2 id="argon-components">Argon Dashboard components</h2>
         <p>
           Besides giving the existing Bootstrap elements a new look, we added
           new ones, so that the interface and consistent and homogenous. Going
