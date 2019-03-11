f461192762dcff68add43ed23f5f77b7505075f6
Manu <emanuelnazare@yahoo.ro>
2019-03-11 19:38:36 +0200

    - Popovers docs is done

--- src/layouts/Documentation/Sections/restyled-components/Popovers.jsx
+++ src/layouts/Documentation/Sections/restyled-components/Popovers.jsx
@@ -4,13 +4,161 @@ import { prism } from "react-syntax-highlighter/dist/styles/prism";
 // reactstrap components
 import { Button, PopoverBody, UncontrolledPopover } from "reactstrap";
 
+const codeExample = `import React from "react";
+// reactstrap components
+import { Button, PopoverBody, UncontrolledPopover } from "reactstrap";
+
+class Popovers extends React.Component {
+  render() {
+    return (
+      <>
+        <Button color="default" id="tooltip876279349" type="button">
+          Popover on top
+        </Button>
+        <UncontrolledPopover placement="top" target="tooltip876279349">
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="default" id="tooltip641002859" type="button">
+          Popover on right
+        </Button>
+        <UncontrolledPopover placement="right" target="tooltip641002859">
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="default" id="tooltip765206973" type="button">
+          Popover on bottom
+        </Button>
+        <UncontrolledPopover placement="bottom" target="tooltip765206973">
+          <PopoverBody>
+            Vivamussagittis lacus vel augue laoreet rutrum faucibus.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="default" id="tooltip523914989" type="button">
+          Popover on left
+        </Button>
+        <UncontrolledPopover placement="left" target="tooltip523914989">
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+      </>
+    );
+  }
+}
+
+export default Popovers;
+`;
+
+const codeVariations = `import React from "react";
+// reactstrap components
+import { Button, PopoverBody, UncontrolledPopover } from "reactstrap";
+
+class Popovers extends React.Component {
+  render() {
+    return (
+      <>
+        <Button color="default" id="tooltip348236073" type="button">
+          Default popover
+        </Button>
+        <UncontrolledPopover
+          placement="top"
+          target="tooltip348236073"
+          className="popover-default"
+        >
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="primary" id="tooltip165342254" type="button">
+          Primary popover
+        </Button>
+        <UncontrolledPopover
+          placement="top"
+          target="tooltip165342254"
+          className="popover-primary"
+        >
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="secondary" id="tooltip538314748" type="button">
+          Secondary popover
+        </Button>
+        <UncontrolledPopover
+          placement="top"
+          target="tooltip538314748"
+          className="popover-secondary"
+        >
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="info" id="tooltip751185789" type="button">
+          Info popover
+        </Button>
+        <UncontrolledPopover
+          placement="top"
+          target="tooltip751185789"
+          className="popover-info"
+        >
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="success" id="tooltip518437705" type="button">
+          Success popover
+        </Button>
+        <UncontrolledPopover
+          placement="top"
+          target="tooltip518437705"
+          className="popover-success"
+        >
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="danger" id="tooltip75038580" type="button">
+          Danger popover
+        </Button>
+        <UncontrolledPopover
+          placement="top"
+          target="tooltip75038580"
+          className="popover-danger"
+        >
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+        <Button color="warning" id="tooltip783693128" type="button">
+          Warning popover
+        </Button>
+        <UncontrolledPopover
+          placement="top"
+          target="tooltip783693128"
+          className="popover-warning"
+        >
+          <PopoverBody>
+            This is a very beautiful popover, show some love.
+          </PopoverBody>
+        </UncontrolledPopover>
+      </>
+    );
+  }
+}
+
+export default Popovers;
+`;
+
 class Popovers extends React.Component {
   render() {
     return (
       <>
         <div className="ct-page-title">
           <h1 className="ct-title" id="content">
-            Navbar
+            Popovers
           </h1>
           <div className="avatar-group mt-3" />
         </div>
@@ -22,14 +170,7 @@ class Popovers extends React.Component {
         <hr />
         <h2 id="example">Example</h2>
         <div className="ct-example">
-          <Button
-            color="default"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip876279349"
-            type="button"
-          >
+          <Button color="default" id="tooltip876279349" type="button">
             Popover on top
           </Button>
           <UncontrolledPopover placement="top" target="tooltip876279349">
@@ -37,14 +178,7 @@ class Popovers extends React.Component {
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="default"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="right"
-            id="tooltip641002859"
-            type="button"
-          >
+          <Button color="default" id="tooltip641002859" type="button">
             Popover on right
           </Button>
           <UncontrolledPopover placement="right" target="tooltip641002859">
@@ -52,14 +186,7 @@ class Popovers extends React.Component {
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="default"
-            data-container="body"
-            data-content="Vivamussagittis lacus vel augue laoreet rutrum faucibus."
-            data-placement="bottom"
-            id="tooltip765206973"
-            type="button"
-          >
+          <Button color="default" id="tooltip765206973" type="button">
             Popover on bottom
           </Button>
           <UncontrolledPopover placement="bottom" target="tooltip765206973">
@@ -67,14 +194,7 @@ class Popovers extends React.Component {
               Vivamussagittis lacus vel augue laoreet rutrum faucibus.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="default"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="left"
-            id="tooltip523914989"
-            type="button"
-          >
+          <Button color="default" id="tooltip523914989" type="button">
             Popover on left
           </Button>
           <UncontrolledPopover placement="left" target="tooltip523914989">
@@ -83,121 +203,99 @@ class Popovers extends React.Component {
             </PopoverBody>
           </UncontrolledPopover>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeExample}
+        </SyntaxHighlighter>
         <h2 id="variations">Variations</h2>
         <div className="ct-example">
-          <Button
-            color="default"
-            data-color="default"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip348236073"
-            type="button"
-          >
+          <Button color="default" id="tooltip348236073" type="button">
             Default popover
           </Button>
-          <UncontrolledPopover placement="top" target="tooltip348236073">
+          <UncontrolledPopover
+            placement="top"
+            target="tooltip348236073"
+            className="popover-default"
+          >
             <PopoverBody>
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="primary"
-            data-color="primary"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip165342254"
-            type="button"
-          >
+          <Button color="primary" id="tooltip165342254" type="button">
             Primary popover
           </Button>
-          <UncontrolledPopover placement="top" target="tooltip165342254">
+          <UncontrolledPopover
+            placement="top"
+            target="tooltip165342254"
+            className="popover-primary"
+          >
             <PopoverBody>
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="secondary"
-            data-color="secondary"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip538314748"
-            type="button"
-          >
+          <Button color="secondary" id="tooltip538314748" type="button">
             Secondary popover
           </Button>
-          <UncontrolledPopover placement="top" target="tooltip538314748">
+          <UncontrolledPopover
+            placement="top"
+            target="tooltip538314748"
+            className="popover-secondary"
+          >
             <PopoverBody>
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="info"
-            data-color="info"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip751185789"
-            type="button"
-          >
+          <Button color="info" id="tooltip751185789" type="button">
             Info popover
           </Button>
-          <UncontrolledPopover placement="top" target="tooltip751185789">
+          <UncontrolledPopover
+            placement="top"
+            target="tooltip751185789"
+            className="popover-info"
+          >
             <PopoverBody>
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="success"
-            data-color="success"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip518437705"
-            type="button"
-          >
+          <Button color="success" id="tooltip518437705" type="button">
             Success popover
           </Button>
-          <UncontrolledPopover placement="top" target="tooltip518437705">
+          <UncontrolledPopover
+            placement="top"
+            target="tooltip518437705"
+            className="popover-success"
+          >
             <PopoverBody>
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="danger"
-            data-color="danger"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip75038580"
-            type="button"
-          >
+          <Button color="danger" id="tooltip75038580" type="button">
             Danger popover
           </Button>
-          <UncontrolledPopover placement="top" target="tooltip75038580">
+          <UncontrolledPopover
+            placement="top"
+            target="tooltip75038580"
+            className="popover-danger"
+          >
             <PopoverBody>
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
-          <Button
-            color="warning"
-            data-color="warning"
-            data-container="body"
-            data-content="This is a very beautiful popover, show some love."
-            data-placement="top"
-            id="tooltip783693128"
-            type="button"
-          >
+          <Button color="warning" id="tooltip783693128" type="button">
             Warning popover
           </Button>
-          <UncontrolledPopover placement="top" target="tooltip783693128">
+          <UncontrolledPopover
+            placement="top"
+            target="tooltip783693128"
+            className="popover-warning"
+          >
             <PopoverBody>
               This is a very beautiful popover, show some love.
             </PopoverBody>
           </UncontrolledPopover>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeVariations}
+        </SyntaxHighlighter>
         <h3 id="examples">Props</h3>
         <p>
           Please refer to{" "}
