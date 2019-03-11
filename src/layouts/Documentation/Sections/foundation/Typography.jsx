419c83e0ea5024a9c5c86589cfb680748112291b
Manu <emanuelnazare@yahoo.ro>
2019-03-11 14:04:55 +0200

    - Typography is done

--- src/layouts/Documentation/Sections/foundation/Typography.jsx
+++ src/layouts/Documentation/Sections/foundation/Typography.jsx
@@ -33,6 +33,165 @@ const codeLead = `<p className="lead">
   auctor. Duis mollis, est non commodo luctus.
 </p>`;
 
+const codeInlineTextElements = `<p>
+  You can use the mark tag to <mark>highlight</mark>
+  text.
+</p>
+<p>
+  <del>This line of text is meant to be treated as deleted text.</del>
+</p>
+<p>
+  <s>
+    This line of text is meant to be treated as no longer accurate.
+  </s>
+</p>
+<p>
+  <ins>
+    This line of text is meant to be treated as an addition to the
+    document.
+  </ins>
+</p>
+<p>
+  <u>This line of text will render as underlined</u>
+</p>
+<p>
+  <small>
+    This line of text is meant to be treated as fine print.
+  </small>
+</p>
+<p>
+  <strong>This line rendered as bold text.</strong>
+</p>
+<p>
+  <em>This line rendered as italicized text.</em>
+</p>`;
+
+const codeAbbreviations = `<p>
+  <abbr title="attribute">attr</abbr>
+</p>
+<p>
+  <abbr className="initialism" title="HyperText Markup Language">
+    HTML
+  </abbr>
+</p>`;
+
+const codeBlockquotes = `<blockquote className="blockquote">
+  <p className="mb-0">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+    posuere erat a ante.
+  </p>
+</blockquote>`;
+
+const codeNamingASource = `<blockquote className="blockquote">
+  <p className="mb-0">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+    posuere erat a ante.
+  </p>
+  <footer className="blockquote-footer">
+    Someone famous in <cite title="Source Title">Source Title</cite>
+  </footer>
+</blockquote>`;
+
+const codeAlignment1 = `<blockquote className="blockquote text-center">
+  <p className="mb-0">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+    posuere erat a ante.
+  </p>
+  <footer className="blockquote-footer">
+    Someone famous in <cite title="Source Title">Source Title</cite>
+  </footer>
+</blockquote>`;
+
+const codeAlignment2 = `<blockquote className="blockquote text-right">
+  <p className="mb-0">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+    posuere erat a ante.
+  </p>
+  <footer className="blockquote-footer">
+    Someone famous in <cite title="Source Title">Source Title</cite>
+  </footer>
+</blockquote>`;
+
+const codeUnstyled = `<ul className="list-unstyled">
+  <li>Lorem ipsum dolor sit amet</li>
+  <li>Consectetur adipiscing elit</li>
+  <li>Integer molestie lorem at massa</li>
+  <li>Facilisis in pretium nisl aliquet</li>
+  <li>
+    Nulla volutpat aliquam velit{" "}
+    <ul>
+      <li>Phasellus iaculis neque</li>
+      <li>Purus sodales ultricies</li>
+      <li>Vestibulum laoreet porttitor sem</li>
+      <li>Ac tristique libero volutpat at</li>
+    </ul>
+  </li>
+  <li>Faucibus porta lacus fringilla vel</li>
+  <li>Aenean sit amet erat nunc</li>
+  <li>Eget porttitor lorem</li>
+</ul>`;
+
+const codeInline = `<ul className="list-inline">
+  <li className="list-inline-item">Lorem ipsum</li>
+  <li className="list-inline-item">Phasellus iaculis</li>
+  <li className="list-inline-item">Nulla volutpat</li>
+</ul>`;
+
+const codeDescriptionListAlignment = `<Row>
+  <Col sm="3">Description lists</Col>
+  <Col sm="9">A description list is perfect for defining terms.</Col>
+  <Col sm="3">Euismod</Col>
+  <Col sm="9">
+    <p>
+      Vestibulum id ligula porta felis euismod semper eget lacinia
+      odio sem nec elit.
+    </p>
+    <p>Donec id elit non mi porta gravida at eget metus.</p>
+  </Col>
+  <Col sm="3">Malesuada porta</Col>
+  <Col sm="9">Etiam porta sem malesuada magna mollis euismod.</Col>
+  <Col className="text-truncate" sm="3">
+    Truncated term is truncated
+  </Col>
+  <Col sm="9">
+    Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
+    nibh, ut fermentum massa justo sit amet risus.
+  </Col>
+  <Col sm="3">Nesting</Col>
+  <Col sm="9">
+    <Row>
+      <Col sm="4">Nested definition list</Col>
+      <Col sm="8">
+        Aenean posuere, tortor sed cursus feugiat, nunc augue blandit
+        nunc.
+      </Col>
+    </Row>
+  </Col>
+</Row>`;
+
+const codeResponsiveTypography = `html {
+  font-size: 1rem;
+}
+
+@include media-breakpoint-up(sm) {
+  html {
+    font-size: 1.2rem;
+  }
+}
+
+@include media-breakpoint-up(md) {
+  html {
+    font-size: 1.4rem;
+  }
+}
+
+@include media-breakpoint-up(lg) {
+  html {
+    font-size: 1.6rem;
+  }
+}`;
+
+
 class Typography extends React.Component {
   render() {
     return (
@@ -244,6 +403,9 @@ class Typography extends React.Component {
             <em>This line rendered as italicized text.</em>
           </p>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeInlineTextElements}
+        </SyntaxHighlighter>
         <p>
           <code className="highlighter-rouge">.mark</code>
           and <code className="highlighter-rouge">.small</code>
@@ -292,6 +454,9 @@ class Typography extends React.Component {
             </abbr>
           </p>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeAbbreviations}
+        </SyntaxHighlighter>
         <h2 id="blockquotes">Blockquotes</h2>
         <p>
           For quoting blocks of content from another source within your
@@ -310,6 +475,9 @@ class Typography extends React.Component {
             </p>
           </blockquote>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeBlockquotes}
+        </SyntaxHighlighter>
         <h3 id="naming-a-source">Naming a source</h3>
         <p>
           Add a{" "}
@@ -330,6 +498,9 @@ class Typography extends React.Component {
             </footer>
           </blockquote>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeNamingASource}
+        </SyntaxHighlighter>
         <h3 id="alignment">Alignment</h3>
         <p>
           Use text utilities as needed to change the alignment of your
@@ -346,6 +517,9 @@ class Typography extends React.Component {
             </footer>
           </blockquote>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeAlignment1}
+        </SyntaxHighlighter>
         <div className="ct-example">
           <blockquote className="blockquote text-right">
             <p className="mb-0">
@@ -357,6 +531,9 @@ class Typography extends React.Component {
             </footer>
           </blockquote>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeAlignment2}
+        </SyntaxHighlighter>
         <h2 id="lists">Lists</h2>
         <h3 id="unstyled">Unstyled</h3>
         <p>
@@ -386,6 +563,9 @@ class Typography extends React.Component {
             <li>Eget porttitor lorem</li>
           </ul>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeUnstyled}
+        </SyntaxHighlighter>
         <h3 id="inline">Inline</h3>
         <p>
           Remove a list’s bullets and apply some light{" "}
@@ -401,6 +581,9 @@ class Typography extends React.Component {
             <li className="list-inline-item">Nulla volutpat</li>
           </ul>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeInline}
+        </SyntaxHighlighter>
         <h3 id="description-list-alignment">Description list alignment</h3>
         <p>
           Align terms and descriptions horizontally by using our grid system’s
@@ -442,6 +625,9 @@ class Typography extends React.Component {
             </Col>
           </Row>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeDescriptionListAlignment}
+        </SyntaxHighlighter>
         <h2 id="responsive-typography">Responsive typography</h2>
         <p>
           <em>Responsive typography</em>
@@ -455,6 +641,9 @@ class Typography extends React.Component {
           <code className="highlighter-rouge">font-size</code>s and media
           queries you wish.
         </p>
+        <SyntaxHighlighter language="css" style={prism}>
+          {codeResponsiveTypography}
+        </SyntaxHighlighter>
       </>
     );
   }
