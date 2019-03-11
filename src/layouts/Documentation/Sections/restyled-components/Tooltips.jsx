6a7befa486addcd5e5b61b77f87b09b29cc61c48
Manu <emanuelnazare@yahoo.ro>
2019-03-11 20:03:23 +0200

    - Tooltip docs is done

--- src/layouts/Documentation/Sections/restyled-components/Tooltips.jsx
+++ src/layouts/Documentation/Sections/restyled-components/Tooltips.jsx
@@ -4,6 +4,90 @@ import { prism } from "react-syntax-highlighter/dist/styles/prism";
 // reactstrap components
 import { Button, UncontrolledTooltip } from "reactstrap";
 
+const codeExample = `import React from "react";
+// reactstrap components
+import { Button, UncontrolledTooltip } from "reactstrap";
+
+class Tooltips extends React.Component {
+  render() {
+    return (
+      <>
+        <Button
+          className="btn-white"
+          color="default"
+          data-placement="top"
+          id="tooltip611234743"
+          size="sm"
+          type="button"
+        >
+          Tooltip on top
+        </Button>
+        <UncontrolledTooltip
+          delay={0}
+          placement="top"
+          target="tooltip611234743"
+        >
+          Tooltip on top
+        </UncontrolledTooltip>
+        <Button
+          className="btn-white"
+          color="default"
+          data-placement="right"
+          id="tooltip159654437"
+          size="sm"
+          type="button"
+        >
+          Tooltip on right
+        </Button>
+        <UncontrolledTooltip
+          delay={0}
+          placement="right"
+          target="tooltip159654437"
+        >
+          Tooltip on right
+        </UncontrolledTooltip>
+        <Button
+          className="btn-white"
+          color="default"
+          data-placement="bottom"
+          id="tooltip860969338"
+          size="sm"
+          type="button"
+        >
+          Tooltip on bottom
+        </Button>
+        <UncontrolledTooltip
+          delay={0}
+          placement="bottom"
+          target="tooltip860969338"
+        >
+          Tooltip on bottom
+        </UncontrolledTooltip>
+        <Button
+          className="btn-white"
+          color="default"
+          data-placement="left"
+          id="tooltip297043631"
+          size="sm"
+          type="button"
+        >
+          Tooltip on left
+        </Button>
+        <UncontrolledTooltip
+          delay={0}
+          placement="left"
+          target="tooltip297043631"
+        >
+          Tooltip on left
+        </UncontrolledTooltip>
+      </>
+    );
+  }
+}
+
+export default Tooltips;
+`;
+
 class Tooltips extends React.Component {
   render() {
     return (
@@ -91,6 +175,9 @@ class Tooltips extends React.Component {
             Tooltip on left
           </UncontrolledTooltip>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeExample}
+        </SyntaxHighlighter>
         <h3 id="examples">Props</h3>
         <p>
           Please refer to{" "}
