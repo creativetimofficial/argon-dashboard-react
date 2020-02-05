622ddf9d13b12c03a21afdf6243c5756b942d9a4
Mahavir Hirani <30505951+mahavirhirani@users.noreply.github.com>
2020-02-05 18:07:09 +0530

    fixed the issue if user only enter "/admin" url
    the issue is if the user only enter "/admin" in the url it currently displaying empty page 
    ================solution ============
    if user enter the only "/admin"  in the url  it should be redirected to the  "/admin/index" (dashboard) route

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -76,7 +76,10 @@ class Admin extends React.Component {
             {...this.props}
             brandText={this.getBrandText(this.props.location.pathname)}
           />
-          <Switch>{this.getRoutes(routes)}</Switch>
+          <Switch>
+            {this.getRoutes(routes)}
+            <Redirect from="*" to="/admin/index" />
+          </Switch>
           <Container fluid>
             <AdminFooter />
           </Container>
