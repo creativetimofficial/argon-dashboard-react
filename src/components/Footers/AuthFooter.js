2b0ca4d223ecc2deaa535571a8a26c2c2b6711ce
EINazare <emanuelioannazare@gmail.com>
2020-02-07 12:19:15 +0200

    refactor(): changed .jsx to .js

---
+++ src/components/Footers/AuthFooter.js
@@ -0,0 +1,87 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <>
+        <footer className="py-5">
+          <Container>
+            <Row className="align-items-center justify-content-xl-between">
+              <Col xl="6">
+                <div className="copyright text-center text-xl-left text-muted">
+                  © 2018{" "}
+                  <a
+                    className="font-weight-bold ml-1"
+                    href="https://www.creative-tim.com?ref=adr-auth-footer"
+                    target="_blank"
+                  >
+                    Creative Tim
+                  </a>
+                </div>
+              </Col>
+              <Col xl="6">
+                <Nav className="nav-footer justify-content-center justify-content-xl-end">
+                  <NavItem>
+                    <NavLink
+                      href="https://www.creative-tim.com?ref=adr-auth-footer"
+                      target="_blank"
+                    >
+                      Creative Tim
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="https://www.creative-tim.com/presentation?ref=adr-auth-footer"
+                      target="_blank"
+                    >
+                      About Us
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="http://blog.creative-tim.com?ref=adr-auth-footer"
+                      target="_blank"
+                    >
+                      Blog
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
+                      target="_blank"
+                    >
+                      MIT License
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default Login;
