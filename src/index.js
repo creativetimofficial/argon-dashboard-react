c1c837062bc2b65c1a02fde288dd25e72d9fc09d
EINazare <emanuelioannazare@gmail.com>
2020-02-07 12:14:23 +0200

    refactor(): changed whole assets folder with Argon Dashboard HTML
    This commit does not comply with our rules of Git Workflow, but due to 
    the fact that the whole folder was changed, it was no point in having 
    1000+ commits.

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.jsx";
