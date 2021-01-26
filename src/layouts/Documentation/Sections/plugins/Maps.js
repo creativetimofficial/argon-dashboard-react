d041baec3cfcfacfb27497caf1a3ad20386710c3
EINazare <emanuelioannazare@gmail.com>
2021-01-26 16:13:23 +0200

    refactor: change react-google-maps to google maps api

--- src/layouts/Documentation/Sections/plugins/Maps.js
+++ src/layouts/Documentation/Sections/plugins/Maps.js
@@ -1,219 +1,55 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/styles/prism";
-// react plugin used to create google maps
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
 
-const MapWrapper = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        styles: [
-          {
-            featureType: "administrative",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#444444" }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "all",
-            stylers: [{ color: "#f2f2f2" }]
-          },
-          {
-            featureType: "poi",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "road",
-            elementType: "all",
-            stylers: [{ saturation: -100 }, { lightness: 45 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "all",
-            stylers: [{ visibility: "simplified" }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "labels.icon",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "transit",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "water",
-            elementType: "all",
-            stylers: [{ color: "#5e72e4" }, { visibility: "on" }]
-          }
-        ]
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
-
-const codeExample = `import React from "react";
-// react plugin used to create google maps
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
-
-const MapWrapper = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        styles: [
-          {
-            featureType: "administrative",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#444444" }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "all",
-            stylers: [{ color: "#f2f2f2" }]
-          },
-          {
-            featureType: "poi",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "road",
-            elementType: "all",
-            stylers: [{ saturation: -100 }, { lightness: 45 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "all",
-            stylers: [{ visibility: "simplified" }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "labels.icon",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "transit",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "water",
-            elementType: "all",
-            stylers: [{ color: "#5e72e4" }, { visibility: "on" }]
-          }
-        ]
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
-
-class Maps extends React.Component {
+class GoogleMaps extends React.Component {
   render() {
     return (
-      <>
-        <MapWrapper
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-          loadingElement={<div style={{ height: \`100%\` }} />}
-          containerElement={
-            <div
-              style={{ height: \`600px\` }}
-              className="map-canvas"
-              id="map-canvas"
-            />
-          }
-          mapElement={
-            <div style={{ height: \`100%\`, borderRadius: "inherit" }} />
-          }
-        />
-      </>
-    );
-  }
-}
-
-export default Maps;
-`;
-
-class Maps extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="ct-page-title">
-          <h1 className="ct-title" id="content">
-            React Google Maps v9.4.5
-          </h1>
-          <div className="avatar-group mt-3" />
-        </div>
-        <p className="ct-lead">
-          Our map component uses the Google Maps API and it comes with a custom
-          interface that matches the theme’s colors
+      <div>
+        <h1>Google Maps</h1>
+        <p className="lead">
+          Our Map components uses the Google Maps API and it comes with a custom
+          interface that matches the theme’s colours. Keep reading our Maps
+          examples and learn how to use this plugin.
         </p>
-        <hr />
-        <h2 id="example">Example</h2>
-        <div className="ct-example">
-          <MapWrapper
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={
-              <div
-                style={{ height: `600px` }}
-                className="map-canvas"
-                id="map-canvas"
-              />
-            }
-            mapElement={
-              <div style={{ height: `100%`, borderRadius: "inherit" }} />
-            }
-          />
-        </div>
+        <h2>Usage</h2>
+        <p>
+          In order to use this plugin on your page you will need to include the
+          following script inside your <code>public/index.html</code> page,
+          right before the end of the <code>{`</head>`}</code> tag:
+        </p>
         <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExample}
+          {`<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE"></script>`}
         </SyntaxHighlighter>
-        <h3 id="get-your-api-key">Get your API key</h3>
+        <h2>Get your API key</h2>
         <p>
           In order to get your Google Maps API key navigate to the following
           page:{" "}
-          <a href="https://developers.google.com/maps/documentation/javascript/get-api-key">
+          <a
+            className="font-medium text-indigo-600"
+            href="https://developers.google.com/maps/documentation/javascript/get-api-key?ref=creativetim"
+            rel="nofollow"
+            target="_blank"
+          >
             Google Maps
           </a>
+          . After you’ve got your api key, you will need to replace in the above
+          script, the <code>YOUR_API_KEY_HERE</code> with that key.
         </p>
-        <h3 id="initialization">Props</h3>
         <p>
-          Please refer to{" "}
+          Read more about Google Maps on their own website:{" "}
           <a
-            href="https://tomchentw.github.io/react-google-maps/"
+            className="font-medium text-indigo-600"
+            href="https://developers.google.com/maps/documentation/javascript/overview?ref=creativetim"
+            rel="nofollow"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            react-google-maps documentation
+            here
           </a>
-          .
         </p>
-      </>
+      </div>
     );
   }
 }
 
-export default Maps;
+export default GoogleMaps;
