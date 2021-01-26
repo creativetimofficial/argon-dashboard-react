f3ee831025f9d1694d88af3920c0671e838283d2
EINazare <emanuelioannazare@gmail.com>
2021-01-26 15:04:06 +0200

    fix: solve new import images

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -149,7 +149,10 @@ const Sidebar = (props) => {
                 <span className="avatar avatar-sm rounded-circle">
                   <img
                     alt="..."
-                    src={require("../../assets/img/theme/team-1-800x800.jpg")}
+                    src={
+                      require("../../assets/img/theme/team-1-800x800.jpg")
+                        .default
+                    }
                   />
                 </span>
               </Media>
