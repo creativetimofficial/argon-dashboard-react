8f398d346889c1607895c364995e4e567371c61f
EINazare <emanuelioannazare@gmail.com>
2021-01-26 15:04:02 +0200

    fix: solve new import images

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -65,7 +65,10 @@ const AdminNavbar = (props) => {
                   <span className="avatar avatar-sm rounded-circle">
                     <img
                       alt="..."
-                      src={require("../../assets/img/theme/team-4-800x800.jpg")}
+                      src={
+                        require("../../assets/img/theme/team-4-800x800.jpg")
+                          .default
+                      }
                     />
                   </span>
                   <Media className="ml-2 d-none d-lg-block">
