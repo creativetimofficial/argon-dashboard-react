f4fabd41c1c3e1c6eeaf9c42b92e22c01301e976
EINazare <emanuelioannazare@gmail.com>
2021-01-26 16:20:46 +0200

    style: run prettier

--- broken-links/documentation/overview/assets/vendor/prismjs/components/index.js
+++ broken-links/documentation/overview/assets/vendor/prismjs/components/index.js
@@ -1,6 +1,5 @@
-const components = require('../components.js');
-const getLoader = require('../dependencies');
-
+const components = require("../components.js");
+const getLoader = require("../dependencies");
 
 /**
  * The set of all languages which have been loaded using the below function.
@@ -18,34 +17,34 @@ const loadedLanguages = new Set();
  * @returns {void}
  */
 function loadLanguages(languages) {
-	if (languages === undefined) {
-		languages = Object.keys(components.languages).filter(l => l != 'meta');
-	} else if (!Array.isArray(languages)) {
-		languages = [languages];
-	}
+  if (languages === undefined) {
+    languages = Object.keys(components.languages).filter((l) => l != "meta");
+  } else if (!Array.isArray(languages)) {
+    languages = [languages];
+  }
 
-	// the user might have loaded languages via some other way or used `prism.js` which already includes some
-	// we don't need to validate the ids because `getLoader` will ignore invalid ones
-	const loaded = [...loadedLanguages, ...Object.keys(Prism.languages)];
+  // the user might have loaded languages via some other way or used `prism.js` which already includes some
+  // we don't need to validate the ids because `getLoader` will ignore invalid ones
+  const loaded = [...loadedLanguages, ...Object.keys(Prism.languages)];
 
-	getLoader(components, languages, loaded).load(lang => {
-		if (!(lang in components.languages)) {
-			if (!loadLanguages.silent) {
-				console.warn('Language does not exist: ' + lang);
-			}
-			return;
-		}
+  getLoader(components, languages, loaded).load((lang) => {
+    if (!(lang in components.languages)) {
+      if (!loadLanguages.silent) {
+        console.warn("Language does not exist: " + lang);
+      }
+      return;
+    }
 
-		const pathToLanguage = './prism-' + lang;
+    const pathToLanguage = "./prism-" + lang;
 
-		// remove from require cache and from Prism
-		delete require.cache[require.resolve(pathToLanguage)];
-		delete Prism.languages[lang];
+    // remove from require cache and from Prism
+    delete require.cache[require.resolve(pathToLanguage)];
+    delete Prism.languages[lang];
 
-		require(pathToLanguage);
+    require(pathToLanguage);
 
-		loadedLanguages.add(lang);
-	});
+    loadedLanguages.add(lang);
+  });
 }
 
 /**
