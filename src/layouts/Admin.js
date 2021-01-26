8e9bb09249e1567518b0a1d9ef397580f21999dc
EINazare <emanuelioannazare@gmail.com>
2021-01-26 15:04:08 +0200

    fix: solve new import images

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -70,7 +70,7 @@ const Admin = (props) => {
         routes={routes}
         logo={{
           innerLink: "/admin/index",
-          imgSrc: require("../assets/img/brand/argon-react.png"),
+          imgSrc: require("../assets/img/brand/argon-react.png").default,
           imgAlt: "...",
         }}
       />
