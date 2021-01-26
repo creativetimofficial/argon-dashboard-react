8d879bd5451e6070f5a4f8a20db8c3452ffe1d07
EINazare <emanuelioannazare@gmail.com>
2021-01-26 16:18:54 +0200

    fix: solve required images

--- src/layouts/Documentation/Sections/core-components/Sidebar.js
+++ src/layouts/Documentation/Sections/core-components/Sidebar.js
@@ -20,7 +20,7 @@ class Admin extends React.Component {
           routes={routes}
           logo={{
             innerLink: "/admin/index",
-            imgSrc: require("assets/img/brand/argon-react.png"),
+            imgSrc: require("assets/img/brand/argon-react.png").default,
             imgAlt: "..."
           }}
         />
