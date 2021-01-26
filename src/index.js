caecc7f4dd7a4b29dd130c922f2c782913c261bf
EINazare <emanuelioannazare@gmail.com>
2021-01-26 15:09:08 +0200

    fix: solve relative to absolute paths

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ import "./assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/scss/argon-dashboard-react.scss";
 
-import AdminLayout from "./layouts/Admin.js";
-import AuthLayout from "./layouts/Auth.js";
+import AdminLayout from "layouts/Admin.js";
+import AuthLayout from "layouts/Auth.js";
 
 ReactDOM.render(
   <BrowserRouter>
