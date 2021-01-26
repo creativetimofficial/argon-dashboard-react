beb4bab817eea748219781d032d9c00b768690be
EINazare <emanuelioannazare@gmail.com>
2021-01-26 15:49:12 +0200

    docs: change dates and version

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* Argon Dashboard React - v1.1.0
+* Argon Dashboard React - v1.2.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
