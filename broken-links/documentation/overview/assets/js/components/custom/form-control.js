f2526c9e71780d1a8535acb81d1b0f540ed9fce5
EINazare <emanuelioannazare@gmail.com>
2021-01-26 16:20:21 +0200

    style: run prettier

--- broken-links/documentation/overview/assets/js/components/custom/form-control.js
+++ broken-links/documentation/overview/assets/js/components/custom/form-control.js
@@ -2,28 +2,28 @@
 // Form control
 //
 
-'use strict';
+"use strict";
 
-var FormControl = (function() {
+var FormControl = (function () {
+  // Variables
 
-	// Variables
+  var $input = $(".form-control");
 
-	var $input = $('.form-control');
+  // Methods
 
+  function init($this) {
+    $this
+      .on("focus blur", function (e) {
+        $(this)
+          .parents(".form-group")
+          .toggleClass("focused", e.type === "focus");
+      })
+      .trigger("blur");
+  }
 
-	// Methods
+  // Events
 
-	function init($this) {
-		$this.on('focus blur', function(e) {
-        $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus'));
-    }).trigger('blur');
-	}
-
-
-	// Events
-
-	if ($input.length) {
-		init($input);
-	}
-
+  if ($input.length) {
+    init($input);
+  }
 })();
