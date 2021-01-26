6170b5be7d01e3b187987715655a9558a8b874fc
EINazare <emanuelioannazare@gmail.com>
2021-01-26 16:23:06 +0200

    style: run prettier

--- src/layouts/Documentation/Sections/getting-started/RoutingSystem.js
+++ src/layouts/Documentation/Sections/getting-started/RoutingSystem.js
@@ -19,10 +19,9 @@ class RoutingSystem extends React.Component {
           in two places. On place would have been inside our{" "}
           <Link to="/documentation/sidebar">Sidebar</Link> and the onther one
           would be either the{" "}
-          <code className="highlighter-rouge">src/layouts/Admin.js</code>{" "}
-          layout or the{" "}
-          <code className="highlighter-rouge">src/Admin/Auth.js</code> layout.
-          You will find all our demo routes in{" "}
+          <code className="highlighter-rouge">src/layouts/Admin.js</code> layout
+          or the <code className="highlighter-rouge">src/Admin/Auth.js</code>{" "}
+          layout. You will find all our demo routes in{" "}
           <code className="highlighter-rouge">src/routes.js</code>
         </p>
         <hr />
