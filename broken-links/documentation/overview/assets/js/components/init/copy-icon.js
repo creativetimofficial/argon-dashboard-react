3aee29f118cbf7b400b66194778cb1c71202874a
EINazare <emanuelioannazare@gmail.com>
2021-01-26 16:20:23 +0200

    style: run prettier

--- broken-links/documentation/overview/assets/js/components/init/copy-icon.js
+++ broken-links/documentation/overview/assets/js/components/init/copy-icon.js
@@ -2,44 +2,40 @@
 // Icon code copy/paste
 //
 
-'use strict';
+"use strict";
 
-var CopyIcon = (function() {
+var CopyIcon = (function () {
+  // Variables
 
-	// Variables
+  var $element = ".btn-icon-clipboard",
+    $btn = $($element);
 
-	var $element = '.btn-icon-clipboard',
-		$btn = $($element);
+  // Methods
 
+  function init($this) {
+    $this.tooltip().on("mouseleave", function () {
+      // Explicitly hide tooltip, since after clicking it remains
+      // focused (as it's a button), so tooltip would otherwise
+      // remain visible until focus is moved away
+      $this.tooltip("hide");
+    });
 
-	// Methods
+    var clipboard = new ClipboardJS($element);
 
-	function init($this) {
-		$this.tooltip().on('mouseleave', function() {
-			// Explicitly hide tooltip, since after clicking it remains
-			// focused (as it's a button), so tooltip would otherwise
-			// remain visible until focus is moved away
-			$this.tooltip('hide');
-		});
+    clipboard.on("success", function (e) {
+      $(e.trigger)
+        .attr("title", "Copied!")
+        .tooltip("_fixTitle")
+        .tooltip("show")
+        .attr("title", "Copy to clipboard")
+        .tooltip("_fixTitle");
 
-		var clipboard = new ClipboardJS($element);
+      e.clearSelection();
+    });
+  }
 
-		clipboard.on('success', function(e) {
-			$(e.trigger)
-				.attr('title', 'Copied!')
-				.tooltip('_fixTitle')
-				.tooltip('show')
-				.attr('title', 'Copy to clipboard')
-				.tooltip('_fixTitle')
-
-			e.clearSelection()
-		});
-	}
-
-
-	// Events
-	if ($btn.length) {
-		init($btn);
-	}
-
+  // Events
+  if ($btn.length) {
+    init($btn);
+  }
 })();
