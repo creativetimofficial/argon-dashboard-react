dbeb1a817ce77cc29dffe45b5f146707f82e2f1e
EINazare <emanuelioannazare@gmail.com>
2021-01-26 15:09:18 +0200

    fix: solve relative to absolute paths

--- src/views/examples/Icons.js
+++ src/views/examples/Icons.js
@@ -29,7 +29,7 @@ import {
   UncontrolledTooltip,
 } from "reactstrap";
 // core components
-import Header from "../../components/Headers/Header.js";
+import Header from "components/Headers/Header.js";
 
 const Icons = () => {
   const [copiedText, setCopiedText] = useState();
