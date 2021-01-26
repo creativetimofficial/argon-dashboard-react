1cf173c1851a981ebf9bce7405e517f4c96b4507
EINazare <emanuelioannazare@gmail.com>
2021-01-26 15:09:16 +0200

    fix: solve relative to absolute paths

--- src/views/Index.js
+++ src/views/Index.js
@@ -44,9 +44,9 @@ import {
   parseOptions,
   chartExample1,
   chartExample2,
-} from "../variables/charts.js";
+} from "variables/charts.js";
 
-import Header from "../components/Headers/Header.js";
+import Header from "components/Headers/Header.js";
 
 const Index = (props) => {
   const [activeNav, setActiveNav] = useState(1);
