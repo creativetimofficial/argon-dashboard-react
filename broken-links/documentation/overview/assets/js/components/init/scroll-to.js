a6e67d10bfea9daecaab376a98929e62f0312737
EINazare <emanuelioannazare@gmail.com>
2021-01-26 16:20:27 +0200

    style: run prettier

--- broken-links/documentation/overview/assets/js/components/init/scroll-to.js
+++ broken-links/documentation/overview/assets/js/components/init/scroll-to.js
@@ -2,43 +2,41 @@
 // Scroll to (anchor links)
 //
 
-'use strict';
+"use strict";
 
-var ScrollTo = (function() {
+var ScrollTo = (function () {
+  //
+  // Variables
+  //
 
-	//
-	// Variables
-	//
+  var $scrollTo = $(".scroll-me, [data-scroll-to], .toc-entry a");
 
-	var $scrollTo = $('.scroll-me, [data-scroll-to], .toc-entry a');
+  //
+  // Methods
+  //
 
+  function scrollTo($this) {
+    var $el = $this.attr("href");
+    var offset = $this.data("scroll-to-offset")
+      ? $this.data("scroll-to-offset")
+      : 0;
+    var options = {
+      scrollTop: $($el).offset().top - offset,
+    };
 
-	//
-	// Methods
-	//
+    // Animate scroll to the selected section
+    $("html, body").stop(true, true).animate(options, 600);
 
-	function scrollTo($this) {
-		var $el = $this.attr('href');
-        var offset = $this.data('scroll-to-offset') ? $this.data('scroll-to-offset') : 0;
-		var options = {
-			scrollTop: $($el).offset().top - offset
-		};
+    event.preventDefault();
+  }
 
-        // Animate scroll to the selected section
-        $('html, body').stop(true, true).animate(options, 600);
+  //
+  // Events
+  //
 
-        event.preventDefault();
-	}
-
-
-	//
-	// Events
-	//
-
-	if ($scrollTo.length) {
-		$scrollTo.on('click', function(event) {
-			scrollTo($(this));
-		});
-	}
-
+  if ($scrollTo.length) {
+    $scrollTo.on("click", function (event) {
+      scrollTo($(this));
+    });
+  }
 })();
